4c8c8958e0acd00b787c72aba8c51c859ab76ed8
Diego Pessoa <diegopessoa12@gmail.com>
2019-07-24 17:09:44 -0300

    Exemplo de cadastro e login em JSF

---
+++ jsf-exemplo/src/main/java/br/edu/ifpb/TesteBean.java
@@ -0,0 +1,11 @@
+package br.edu.ifpb;
+
+import javax.faces.bean.*;
+
+@ManagedBean
+@RequestScoped
+public class TesteBean {
+
+    private String nome;
+
+}
---
+++ jsf-exemplo/src/main/java/br/edu/ifpb/model/Usuario.java
@@ -0,0 +1,69 @@
+package br.edu.ifpb.model;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private Integer idade;
+    private String email;
+    private String login;
+    private String senha;
+
+    private Endereco endereco = new Endereco();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
